65afd739d8fb065e493f4953d9a747f9dd0fbb82
djghis <djghis@hotmail.com>
2020-04-08 15:28:37 +0100

    whiskyByYear and DistilleryByRegin working

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -1,10 +1,32 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import com.sun.org.apache.regexp.internal.RE;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(value = "/whiskies")
+//@RequestMapping(value = "/whiskies")
 public class WhiskyController {
+
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping(value = "/whiskies")
+    public ResponseEntity<List<Whisky>> findAllWhiskies(){
+        return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/whiskies/year/{year}")
+    public ResponseEntity<List<Whisky>> getWhiskyByYear(@PathVariable int year){
+        return new ResponseEntity<>(whiskyRepository.findByYear(year), HttpStatus.OK);
+    }
+
+
 
 }
